c9e7786ea50983c44efb04b364e49aafe97ffabc
Makarovks320 <kirill.makarov@tafs.pro>
2020-02-28 17:27:21 +0300

    Updates

---
+++ precache-manifest.82043dca555f61427572d2dd438166e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a600f1ed63b731e29f00160e281987",
+    "url": "/SocialNetworkDemo/index.html"
+  },
+  {
+    "revision": "593ca890109cb8954b2a",
+    "url": "/SocialNetworkDemo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b6cc21436731277046ad",
+    "url": "/SocialNetworkDemo/static/css/3.ad2ad4fb.chunk.css"
+  },
+  {
+    "revision": "3136ac70eac09d82997d",
+    "url": "/SocialNetworkDemo/static/css/main.0e61f736.chunk.css"
+  },
+  {
+    "revision": "593ca890109cb8954b2a",
+    "url": "/SocialNetworkDemo/static/js/2.096f45a2.chunk.js"
+  },
+  {
+    "revision": "b6cc21436731277046ad",
+    "url": "/SocialNetworkDemo/static/js/3.afe5621a.chunk.js"
+  },
+  {
+    "revision": "3136ac70eac09d82997d",
+    "url": "/SocialNetworkDemo/static/js/main.0f9f4f5f.chunk.js"
+  },
+  {
+    "revision": "cf86d39c550d6433b758",
+    "url": "/SocialNetworkDemo/static/js/runtime-main.71fcdc0a.js"
+  },
+  {
+    "revision": "a759d587d33cd65866fc0739a6c85ee5",
+    "url": "/SocialNetworkDemo/static/media/bootstrap-logo.a759d587.svg"
+  },
+  {
+    "revision": "e7119e2a3b3a15136309c2100dd82ceb",
+    "url": "/SocialNetworkDemo/static/media/genericUser.e7119e2a.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/SocialNetworkDemo/static/media/react-logo.25bf045c.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/SocialNetworkDemo/static/media/redux-logo.aa0a3376.svg"
+  }
+]);
